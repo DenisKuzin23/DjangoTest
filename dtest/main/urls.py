f011fc8af735317cb0c4f2d56bb32931e9ee62f2
DenisKuzin23 <gyroin@yandex.ru>
2023-04-24 23:48:56 +0800

    done

--- dtest/main/urls.py
+++ dtest/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.news, name='/')
+    path('', views.news),
+    path('<int:id>', views.new, name='new')
 ]
--- dtest/main/views.py
+++ dtest/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from . import models
 
+
 def news(request):
-    return render(request, 'news.html', {'post_list' : models.Post.objects.all()})
+    return render(request, 'news.html', {'post_list' : models.Post.objects.order_by('-date')})
+
+
+def new(request, id):
+    return render(request, 'new.html', {'post' : models.Post.objects.get(id=id)})
